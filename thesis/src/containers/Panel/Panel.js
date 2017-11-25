c7cf347e269d804149a5f0999f2b7c3aa2a4c081
vidocco <jack.mk.thibaut@gmail.com>
2017-11-25 18:32:23 +0100

    feat: basic panel structure

--- thesis/src/containers/Panel/Panel.js
+++ thesis/src/containers/Panel/Panel.js
@@ -10,13 +10,36 @@ class Panel extends Component {
         <div className="PanelPosition">
           <div className="PanelContainer">
             <div className="PanelSquare">
+              <div className="Wrapper">
+                <h5>Remaining Zen</h5>
+                <div className="Currency">
+                  <h1>173</h1>
+                  <h2>ż</h2>
+                </div>
+              </div>
             </div>
             <div className="PanelSquare">
+              <div className="Wrapper">
+                <h5>Available Zen</h5>
+                <div className="Currency">
+                  <h1>1209</h1>
+                  <h2>ż</h2>
+                </div>
+              </div>
             </div>
             <div className="PanelSquare">
+              <img
+                className="PanelImg"
+                alt="settings"
+                src={require('../../assets/cart.svg')}
+              />
             </div>
-            <div className="PanelSquare">
-              <img alt="settings" src={require('../../assets/settings.svg')}/>
+            <div className="PanelSquare Settings">
+              <img
+                className="PanelImg"
+                alt="settings"
+                src={require('../../assets/settings.svg')}
+              />
             </div>
           </div>
           <h6>powered by Zendama</h6>
