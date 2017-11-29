d92c0c37b502cfcc66d1e7f2fe02ea4af06692e7
vidocco <jack.mk.thibaut@gmail.com>
2017-11-29 20:24:42 +0100

    chore: moved basic ProductList structure to ProductList component

--- thesis/src/containers/Catalog/Catalog.js
+++ thesis/src/containers/Catalog/Catalog.js
@@ -6,6 +6,8 @@ import Loader from '../../components/Loader/Loader';
 import Close from '../../components/Close/Close';
 // import DropDown from '../../components/DropDown/DropDown';
 
+import ProductList from '../../components/ProductList/ProductList';
+
 import '../../App.css';
 import './Catalog.css';
 
@@ -54,69 +56,8 @@ class Catalog extends Component {
             <Close link="/panel"/>
           </div>
           <div className="ProductCatalog">
-            <div className="ProductCategory">
-              <h5>Products</h5>
-              <div className="ProductList">
-                <div className="ProductItem">
-                  <div className="ProductDescription">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla.</p>
-                    <h6>Price: 200</h6>
-                  </div>
-                  <div className="ProductItemImgWrapper">
-                    <img
-                      className="ProductItemImg"
-                      alt="product"
-                      src="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
-                    />
-                  </div>
-                  <h6>Smiley Face</h6>
-                  <input
-                    className="BuyButton"
-                    type="submit"
-                    value="buy"
-                  />
-                </div>
-                <div className="ProductItem">
-                </div>
-                <div className="ProductItem">
-                </div>
-                <div className="ProductItem">
-                </div>
-                <div className="ProductItem">
-                </div>
-              </div>
-              <div className="ArrowContainer">
-                <img
-                  className="MoreContent"
-                  src={require('../../assets/next.svg')}
-                  alt="more content"
-                />
-              </div>
-            </div>
-            <div className="ProductCategory">
-              <h5>Services</h5>
-              <div className="ProductList">
-                <div className="ProductItem">
-                </div>
-                <div className="ProductItem">
-                </div>
-                <div className="ProductItem">
-                </div>
-                <div className="ProductItem">
-                </div>
-                <div className="ProductItem">
-                </div>
-                <div className="ProductItem">
-                </div>
-              </div>
-              <div className="ArrowContainer">
-                <img
-                  className="MoreContent"
-                  src={require('../../assets/next.svg')}
-                  alt="more content"
-                />
-              </div>
-            </div>
+            <ProductList title="Products"/>
+            <ProductList title="Products"/>
           </div>
           <h6>powered by Zendama</h6>
         </div>
