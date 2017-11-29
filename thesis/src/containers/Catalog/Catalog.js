c6282d4bb909914cae69d2a025bf711c56564181
vidocco <jack.mk.thibaut@gmail.com>
2017-11-29 20:17:15 +0100

    feat: basic product item structure

--- thesis/src/containers/Catalog/Catalog.js
+++ thesis/src/containers/Catalog/Catalog.js
@@ -58,6 +58,23 @@ class Catalog extends Component {
               <h5>Products</h5>
               <div className="ProductList">
                 <div className="ProductItem">
+                  <div className="ProductDescription">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla.</p>
+                    <h6>Price: 200</h6>
+                  </div>
+                  <div className="ProductItemImgWrapper">
+                    <img
+                      className="ProductItemImg"
+                      alt="product"
+                      src="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+                    />
+                  </div>
+                  <h6>Smiley Face</h6>
+                  <input
+                    className="BuyButton"
+                    type="submit"
+                    value="buy"
+                  />
                 </div>
                 <div className="ProductItem">
                 </div>
@@ -67,6 +84,13 @@ class Catalog extends Component {
                 </div>
                 <div className="ProductItem">
                 </div>
+              </div>
+              <div className="ArrowContainer">
+                <img
+                  className="MoreContent"
+                  src={require('../../assets/next.svg')}
+                  alt="more content"
+                />
               </div>
             </div>
             <div className="ProductCategory">
@@ -84,6 +108,13 @@ class Catalog extends Component {
                 </div>
                 <div className="ProductItem">
                 </div>
+              </div>
+              <div className="ArrowContainer">
+                <img
+                  className="MoreContent"
+                  src={require('../../assets/next.svg')}
+                  alt="more content"
+                />
               </div>
             </div>
           </div>
