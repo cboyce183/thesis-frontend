dd4846c2e93624ebbb02dde86f9ec84575d24278
vidocco <jack.mk.thibaut@gmail.com>
2017-11-29 20:30:42 +0100

    chore: ProductItem structure moved to ProductItem component

---
+++ thesis/src/components/ProductItem/ProductItem.js
@@ -0,0 +1,33 @@
+import React, { Component, } from 'react';
+
+import '../../App.css';
+import './ProductItem.css';
+
+class ProductItem extends Component {
+
+  render() {
+    return (
+      <div className="ProductItem">
+        <div className="ProductDescription">
+          <p>{this.props.description}</p>
+          <h6>Price: {this.props.price}</h6>
+        </div>
+        <div className="ProductItemImgWrapper">
+          <img
+            className="ProductItemImg"
+            alt="product"
+            src={this.props.image}
+          />
+        </div>
+        <h6>{this.props.title}</h6>
+        <input
+          className="BuyButton"
+          type="submit"
+          value="buy"
+        />
+      </div>
+    );
+  }
+}
+
+export default ProductItem;
--- thesis/src/components/ProductList/ProductList.js
+++ thesis/src/components/ProductList/ProductList.js
@@ -3,6 +3,8 @@ import React, { Component, } from 'react';
 import '../../App.css';
 import './ProductList.css';
 
+import ProductItem from '../ProductItem/ProductItem';
+
 class ProductList extends Component {
 
   render() {
@@ -10,33 +12,54 @@ class ProductList extends Component {
       <div className="ProductCategory">
         <h5>{this.props.title}</h5>
         <div className="ProductList">
-          <div className="ProductItem">
-            <div className="ProductDescription">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla.</p>
-              <h6>Price: 200</h6>
-            </div>
-            <div className="ProductItemImgWrapper">
-              <img
-                className="ProductItemImg"
-                alt="product"
-                src="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
-              />
-            </div>
-            <h6>Smiley Face</h6>
-            <input
-              className="BuyButton"
-              type="submit"
-              value="buy"
-            />
-          </div>
-          <div className="ProductItem">
-          </div>
-          <div className="ProductItem">
-          </div>
-          <div className="ProductItem">
-          </div>
-          <div className="ProductItem">
-          </div>
+          <ProductItem
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla."
+            price="200"
+            image="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+            title="Smiley Face"
+          />
+          <ProductItem
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla."
+            price="200"
+            image="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+            title="Smiley Face"
+          />
+          <ProductItem
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla."
+            price="200"
+            image="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+            title="Smiley Face"
+          />
+          <ProductItem
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla."
+            price="200"
+            image="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+            title="Smiley Face"
+          />
+          <ProductItem
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla."
+            price="200"
+            image="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+            title="Smiley Face"
+          />
+          <ProductItem
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla."
+            price="200"
+            image="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+            title="Smiley Face"
+          />
+          <ProductItem
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla."
+            price="200"
+            image="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+            title="Smiley Face"
+          />
+          <ProductItem
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus, est ac volutpat bibendum, risus lorem aliquet, bla bla bla bla bla bla."
+            price="200"
+            image="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+            title="Smiley Face"
+          />
         </div>
         <div className="ArrowContainer">
           <img
