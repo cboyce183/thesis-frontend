04187fef191cfe26c47cec5521658d49d51300b2
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2017-11-29 16:05:32 +0100

    feat: animated doughnut charts added with % in the center

--- thesis/src/containers/UserWallet/UserWallet.js
+++ thesis/src/containers/UserWallet/UserWallet.js
@@ -1,15 +1,17 @@
 import React, { Component, } from 'react';
-
+import PieChart from 'react-minimal-pie-chart';
+import './UserWallet.css'
 class UserWallet extends Component {
   constructor(props){
     super(props)
     this.state = {
       token:  '',
+      accountInfo: null
     }
   }
 
   getInit(){
-    fetch('http://192.168.0.37:3000/info', {
+    fetch('https://private-3a61ed-zendama.apiary-mock.com/user-wallet', {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
@@ -17,7 +19,7 @@ class UserWallet extends Component {
         'Authorization': 'Bearer ' + window.localStorage.getItem('token'),}
     })
       .then(r => r.json())
-      .then(res => console.log(res))
+      .then(res => this.setState({accountInfo: res}))
   }
 
   componentDidMount(){
@@ -27,10 +29,48 @@ class UserWallet extends Component {
 
 
   render() {
-    console.log('this', window.localStorage.getItem('token'))
+    console.log('this', this.state)
     return (
-      <div className="Container">
-            hello
+      <div className="WalletContainer">
+        <div className="PannelContainer"></div>
+        <div className="DashContainer">
+          <div className="HeaderContainer">
+
+          </div>
+          <div className="SummaryContainer">
+            <div className="SummaryDividerPie">
+              <PieChart className="PercentGiven"
+                startAngle={270}
+                radius={50}
+                lineWidth={20}
+                paddingAngle={3}
+                // rounded={true}
+                animate={true}
+                data={[
+                  { value: 100, key: 1, color: '#CDCDCD',},
+                  { value: 400, key: 2, color: 'black',},]}
+              >
+                <div className="PerGivenText">25%</div>
+              </PieChart>
+              <PieChart className="PercentGiven"
+                startAngle={270}
+                radius={50}
+                lineWidth={20}
+                paddingAngle={3}
+                // rounded={true}
+                animate={true}
+                data={[
+                  { value: 100, key: 1, color: '#CDCDCD',},
+                  { value: 400, key: 2, color: 'black',},]}
+              >
+                <div className="PerRecivedText">25%</div>
+              </PieChart>
+            </div>
+            <div className="SummaryDividerTot"></div>
+          </div>
+          <div className="SheetContainer"></div>
+        </div>
+
       </div>
     )
   }
