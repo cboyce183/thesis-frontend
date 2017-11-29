b913e4f1e865fa3c67eada9f801c6e578a035aee
vidocco <jack.mk.thibaut@gmail.com>
2017-11-29 11:45:46 +0100

    refactor: commented Link in Panel.js

--- thesis/src/containers/Panel/Panel.js
+++ thesis/src/containers/Panel/Panel.js
@@ -1,6 +1,6 @@
 import React, { Component, } from 'react';
 
-import { Link, } from 'react-router-dom';
+// import { Link, } from 'react-router-dom';
 
 import PanelSquare from '../../components/PanelSquare/PanelSquare';
 import NextSteps from '../../components/NextSteps/NextSteps';
