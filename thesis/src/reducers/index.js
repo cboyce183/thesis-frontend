3160cb992fef22fa2d72b632aa542209a2bb8270
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2017-12-01 00:53:12 +0100

    fix: removed redux for user signin form action, index, type, and reducer file

--- thesis/src/reducers/index.js
+++ thesis/src/reducers/index.js
@@ -2,14 +2,6 @@ import * as types from '../actions/types';
 
 const defaultState = {
   saveCompanyInfo: null,
-  UserInfo: {
-    password1: null,
-    password2: null,
-    username: null,
-    email: null,
-  },
-  UserImage: null,
-  TransactionList: [1,2,],
 }
 
 export default (state = defaultState, action) => {
@@ -18,42 +10,6 @@ export default (state = defaultState, action) => {
     return {
       ...state,
       saveCompanyInfo: action.data,
-    }
-  case types.CROPPED_USER_IMAGE:
-    return {
-      ...state,
-      UserImage: action.image,
-    }
-  case types.SET_USER_EMAIL:
-    return {
-      ...state, UserInfo: {
-        ...state.UserInfo,
-        email: action.infoEmail,
-      },
-    }
-  case types.SET_USER_PASSWORD1:
-    return {
-      ...state, UserInfo: {...state.UserInfo, password1: action.infoPass1,},
-    }
-  case types.SET_USER_PASSWORD2:
-    return {
-      ...state, UserInfo: {...state.UserInfo, password2: action.infoPass2,},
-    }
-  case types.SET_USER_USERNAME:
-    return {
-      ...state, UserInfo: {...state.UserInfo, username: action.infoUser,},
-    }
-  case types.CLEAR_EMAIL:
-    return {
-      ...state, UserInfo: {...state.UserInfo, email: '',},
-    }
-  case types.CLEAR_PASSWORD1:
-    return {
-      ...state, UserInfo: {...state.UserInfo, password1: '',},
-    }
-  case types.CLEAR_PASSWORD2:
-    return {
-      ...state, UserInfo: {...state.UserInfo, password2: '',},
     }
   default:
     return state;
