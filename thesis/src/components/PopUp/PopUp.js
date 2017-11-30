a4bb8edaf0b00e8a2b3022aadd5be05be026f1cd
vidocco <jack.mk.thibaut@gmail.com>
2017-11-30 09:36:41 +0100

    chore: created PopUp component

---
+++ thesis/src/components/PopUp/PopUp.js
@@ -0,0 +1,16 @@
+import React, { Component, } from 'react';
+
+import '../../App.css';
+import './ProductItem.css';
+
+class PopUp extends Component {
+
+  render() {
+    return (
+      <div className="PopUp">
+      </div>
+    );
+  }
+}
+
+export default PopUp;
