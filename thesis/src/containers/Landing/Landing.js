9eb66557bec5c1944b649af6de2a4f4ad6cdcdb5
vidocco <jack.mk.thibaut@gmail.com>
2017-12-04 16:53:08 +0100

    style: banner image for landing page

--- thesis/src/containers/Landing/Landing.js
+++ thesis/src/containers/Landing/Landing.js
@@ -46,52 +46,82 @@ class Landing extends Component {
 
   render() {
     return (
-      <div className="landing-wrapper">
-        <div className="landing-title">
-          <img alt="" src={require('../../assets/zendomologo.png')} style={{maxHeight:'80px'}}/>
-          <p className='landing-desc-alt-txt spacer'>spacer</p>
-          <h1 className="landing-title-text" style={{color:'#aaa',}}>zendama</h1>
-        </div>
-        <div className='landing-desc'>
-          <p className='landing-desc-txt'>Help your colleagues, receive rewards, share the love!</p>
-          <p className='landing-desc-alt-txt spacer'>spacer</p>
-          <p className='landing-desc-alt-txt'>Zendama is a catalyst for harmony and friendship in the buisness place.</p>
-          <p className='landing-desc-alt-txt'>Get rewards from your place of work for being a good employee and helping out.</p>
-          <p className='landing-desc-alt-txt'>Reward those who help you with something that will really make a difference.</p>
-          <p className='landing-desc-alt-txt spacer'>spacer</p>
-          <p className='landing-desc-alt-txt' style={{paddingBottom:'3vh',}}>Sounds good right?</p>
-          <input className="u-full-width" style={{width:'50%',}}
-            type="email"
-            value={this.state.email}
-            placeholder="Email"
-            onFocus={() => this.setState({noAccess: false, email: '',})}
-            onChange={(e) => this.setState({email: e.target.value,})}
-          />
-          <input type="password" className="u-full-width" style={{width:'50%', marginBottom:'3vh',}}
-            value={this.state.password}
-            placeholder="Password"
-            onFocus={() => this.setState({noAccess: false, password: '',})}
-            onChange={(e) => this.setState({password: e.target.value,})}
-          />
-          <div className='landing-button'style={{transform:'scale(1.2)',}} onClick={this.loginRequest.bind(this, this.state)}>Login</div>
-          <p className='landing-desc-alt-txt spacer'>spacer</p>
-        </div>
-        <div className='landing-business'>
-          <h5 className='landing-desc-alt-txt'>Get Started</h5>
-          <p className='landing-desc-alt-txt spacer'>spacer</p>
-          <div style={{display:'flex',flexDirection:'row',}}>
-            <div className='landing-button' onClick={this.corporateRedir}>Corporate</div>
-            <div className='landing-button' onClick={this.personalRedir}>Personal</div>
+      <div className="LandingHeader">
+        <div className="LandingTitle">
+          <div className="LandingForm">
+            <div className="LandingLogo">
+              {/* <img alt="ZendamaLogo" className="LandingLogoImg" src={require('../../assets/zendomologo.png')} style={{maxHeight:'80px',}}/> */}
+              {/* <h1 className="landing-title-text" style={{color:'#aaa',}}>Zendama</h1> */}
+            </div>
+            <div className="LogoTextWrap">
+              <h1>Go together if you want to go far</h1>
+            </div>
+            <div className="LoginForm">
+              <p>Already have an account?</p>
+              <input className="InputLogin"
+                type="email"
+                value={this.state.email}
+                placeholder="Email"
+                onFocus={() => this.setState({noAccess: false, email: '',})}
+                onChange={(e) => this.setState({email: e.target.value,})}
+              />
+              <input type="password" className="InputLogin"
+                value={this.state.password}
+                placeholder="Password"
+                onFocus={() => this.setState({noAccess: false, password: '',})}
+                onChange={(e) => this.setState({password: e.target.value,})}
+              />
+              <input type="submit" className='landing-button' onClick={this.loginRequest.bind(this, this.state)} value="login" />
+            </div>
           </div>
         </div>
-        <div className='landing-bottom-bar'>
-          <img alt="" src={require('../../assets/github-white.svg')} style={{maxHeight:'25px',}}/>
-          <p className='landing-desc-alt-txt smallspacer'>spaaaaa</p>
-          <img alt="" src={require('../../assets/facebook-black-icon.png')} style={{maxHeight:'35px',}}/>
-          <p className='landing-desc-alt-txt smallspacer'>spaaaaa</p>
-          <img alt="" src={require('../../assets/twitter.svg')} style={{maxHeight:'25px',}}/>
-        </div>
       </div>
+      // <div className="landing-wrapper">
+      //   <div className="landing-title">
+      //     <img alt="" src={require('../../assets/zendomologo.png')} style={{maxHeight:'80px'}}/>
+      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
+      //     <h1 className="landing-title-text" style={{color:'#aaa',}}>zendama</h1>
+      //   </div>
+      //   <div className='landing-desc'>
+      //     <p className='landing-desc-txt'>Help your colleagues, receive rewards, share the love!</p>
+      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
+      //     <p className='landing-desc-alt-txt'>Zendama is a catalyst for harmony and friendship in the buisness place.</p>
+      //     <p className='landing-desc-alt-txt'>Get rewards from your place of work for being a good employee and helping out.</p>
+      //     <p className='landing-desc-alt-txt'>Reward those who help you with something that will really make a difference.</p>
+      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
+      //     <p className='landing-desc-alt-txt' style={{paddingBottom:'3vh',}}>Sounds good right?</p>
+      //     <input className="u-full-width" style={{width:'50%',}}
+      //       type="email"
+      //       value={this.state.email}
+      //       placeholder="Email"
+      //       onFocus={() => this.setState({noAccess: false, email: '',})}
+      //       onChange={(e) => this.setState({email: e.target.value,})}
+      //     />
+      //     <input type="password" className="u-full-width" style={{width:'50%', marginBottom:'3vh',}}
+      //       value={this.state.password}
+      //       placeholder="Password"
+      //       onFocus={() => this.setState({noAccess: false, password: '',})}
+      //       onChange={(e) => this.setState({password: e.target.value,})}
+      //     />
+      //     <div className='landing-button'style={{transform:'scale(1.2)',}} onClick={this.loginRequest.bind(this, this.state)}>Login</div>
+      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
+      //   </div>
+      //   <div className='landing-business'>
+      //     <h5 className='landing-desc-alt-txt'>Get Started</h5>
+      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
+      //     <div style={{display:'flex',flexDirection:'row',}}>
+      //       <div className='landing-button' onClick={this.corporateRedir}>Corporate</div>
+      //       <div className='landing-button' onClick={this.personalRedir}>Personal</div>
+      //     </div>
+      //   </div>
+      //   <div className='landing-bottom-bar'>
+      //     <img alt="" src={require('../../assets/github-white.svg')} style={{maxHeight:'25px',}}/>
+      //     <p className='landing-desc-alt-txt smallspacer'>spaaaaa</p>
+      //     <img alt="" src={require('../../assets/facebook-black-icon.png')} style={{maxHeight:'35px',}}/>
+      //     <p className='landing-desc-alt-txt smallspacer'>spaaaaa</p>
+      //     <img alt="" src={require('../../assets/twitter.svg')} style={{maxHeight:'25px',}}/>
+      //   </div>
+      // </div>
     );
   }
 }
