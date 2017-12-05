69f5cfcdfb843145bbae03b0aa7853f2fbd6e532
vidocco <jack.mk.thibaut@gmail.com>
2017-12-05 17:15:20 +0100

    refactor: renamed handler and render functions, deleted PasswordValid and EmailValid methods, re-used warning booleans for validation check, refactored arrow functions within render into separate handleFieldChange method

--- thesis/src/containers/UserSignup/UserSignup.js
+++ thesis/src/containers/UserSignup/UserSignup.js
@@ -2,44 +2,66 @@ import React, { Component, } from 'react';
 import { NavLink, } from 'react-router-dom'
 import './UserSignup.css'
 import Crop from './../Cropping/Cropping'
+import base64 from 'base-64';
 
 class UserSignup extends Component {
   constructor(props){
     super(props)
     this.state = {
-      passwordsValid: false,
       passwordWarning: false,
-      emailValid: false,
       emailWarning: false,
       croppingLoad: false,
       popped: false,
-      UserImage: null,
-      UserEmail: null,
-      UserPassword1: null,
-      UserPassword2: null,
-      UserName: null,
-      firstName: null,
-      lastName: null,
+      UserImage: '',
+      email: '',
+      password: '',
+      password2: '',
+      userName: '',
+      firstName: '',
+      lastName: '',
     }
   }
 
+  handleSignUpProcess = () => {
+    this.handleValidPassword();
+    this.handleValidEmail();
+    this.handleUserSignUp(this.state);
+  }
 
-  async userSignupRequest(theProps){
-    if (this.state.passwordsValid && this.state.emailValid && theProps.UserPassword2 !== '') {
+  handleValidPassword = () => {
+    if (this.state.password === this.state.password2) {
+      this.setState({passwordWarning: false,})
+    } else {
+      this.setState({passwordWarning: true,})
+    }
+  }
+
+  handleValidEmail = () => {
+    // regex email expression, which should cover 99% of cases.
+    var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    const bool = re.test(this.state.email)
+    if (bool) {
+      this.setState({emailWarning: false,});
+    }
+    else {
+      this.setState({emailWarning: true,});
+    }
+  }
+
+  handleUserSignUp = () => {
+    if (!this.state.passwordWarning && !this.state.emailWarning && this.state.password2 !== '') {
       const UserDataObject = {
-        profilePic:  theProps.UserImage,
-        email:  theProps.UserEmail,
-        password:  theProps.UserPassword2,
-        username: theProps.UserName,
-        firstName: theProps.firstName,
-        lastName: theProps.lastName,
+        profilePic:  this.state.UserImage,
+        email:  this.state.email,
+        password:  this.state.password2,
+        userName: this.state.userName,
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
       }
-      const query = await window.location.href.match(/user-id=(.*)$/)[1];
+      const query = window.location.href.match(/user-id=(.*)$/)[1];
       if(!window.location.href.match(/user-id=(.*)$/)){
         // window.location.replace('/about_personal')
       }
-      console.log(query);
-
       fetch(`http://192.168.0.37:4200/signup-user?user-id=${query}`, {
         method: 'POST',
         headers: {
@@ -51,79 +73,26 @@ class UserSignup extends Component {
       })
         .then(response => {
           if (response.status === 401) {
-            console.log('error you er fucked')
+            alert('unauthorized access');
           }
-          if (response.status === 200){
-            window.location = '/login'
+          if (response.status === 200) {
+            let headers = new Headers();
+            headers.append('Authorization', `Basic ${base64.encode(`${this.state.email}:${this.state.password}`)}`);
+            fetch('http://192.168.0.37:4200/login', {
+              headers: headers,
+              mode: 'cors',
+            })
+              .then(res => res.json())
+              .then(res => {
+                window.localStorage.setItem('token', res.token)
+                window.location = '/panel';
+              })
+              .catch(e => console.error(e));
           }
         })
     } else {
       console.error('error cannot send!!!')
     }
-  }
-
-  arePasswordsTheSame(theProps){
-    if (theProps.UserPassword1 === theProps.UserPassword2) {
-      this.setState({passwordsValid: true,})
-    } else {
-      this.setState({passwordWarning: true,})
-    }
-  }
-
-  removePasswordWarning(){
-    this.setState({passwordWarning: false,})
-  }
-  removeEmailIsInvalid(){
-    this.setState({emailWarning: false,})
-  }
-
-  warningPassWordNotEqual(bool){
-    if(bool) return (<div> Passwords are not equal. </div>)
-  }
-
-  warningEmailInvalid(bool){
-    if(bool) return (<div> Email is invalid. </div>)
-  }
-
-  imageProfileOrNot(bool){
-    if (!bool) {
-      var reader = new FileReader();
-      reader.readAsDataURL(this.state.profilePic[0]);
-      reader.onloadend = () => {
-        this.setState({base64Image: reader.result,})
-      }
-    }
-  }
-
-  checkEmailValid(email){
-    // regex email expression, which should cover 99% of cases.
-    var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-    const bool = re.test(email)
-    if (bool) {
-      this.setState({emailValid: true,});
-    }
-    else {
-      this.setState({emailWarning: true,});
-    }
-  }
-
-  imageChange(image){
-    if (!image) {
-      return <div className="ProfilePicBox"><p>Upload profile picture</p></div>
-    } else {
-      return <img alt="" src={image} className="ProfilePic"style={{maxHeight:'200px'}}/>
-    }
-  }
-
-
-  removeEmail = () => {
-    this.setState({UserEmail: '',})
-    this.removeEmailIsInvalid()
-  }
-
-  removePassword = (itemVal) => {
-    this.setState({[itemVal]: '',})
-    this.removePasswordWarning()
   }
 
   handlePopUp = (image) => {
@@ -133,7 +102,29 @@ class UserSignup extends Component {
     })
   }
 
-  croppingPopUp(){
+  handleFieldChange = (event) => {
+    this.setState({[event.target.name]: event.target.value,})
+  }
+
+  //=============================================RENDERING
+
+  renderPasswordWarning = () => {
+    if (this.state.passwordWarning) return (<div> Passwords don't match. </div>)
+  }
+
+  renderEmailWarning = () => {
+    if (this.state.emailWarning) return (<div> Invalid e-mail. </div>)
+  }
+
+  renderImageChange = (image) => {
+    if (!image) {
+      return <div className="ProfilePicBox"><p>Upload a profile picture</p></div>
+    } else {
+      return <img alt="" src={image} className="ProfilePic"style={{maxHeight:'200px'}}/>
+    }
+  }
+
+  renderCroppingPopUp = () => {
     if (this.state.popped) {
       return (
         <Crop unpop={this.handlePopUp.bind(this)}/>
@@ -144,79 +135,78 @@ class UserSignup extends Component {
   render() {
     return (
       <div>
-        {this.croppingPopUp()}
+        {this.renderCroppingPopUp()}
         <div className="Container">
           <div className="MainPannel">
             <div className="UserInputContainer">
-              <h4 style={{alignSelf:'center', paddingBottom:'5vh'}}>User Sign-up</h4>
+              <h4 style={{alignSelf:'center', paddingBottom:'5vh',}}>User Sign-up</h4>
               <div className="UserInputBox">
                 <div className="LoginDetails">
-                  <input className="u-full-width"
-                    type="email"
+                  <input
+                    type="text"
+                    name="firstName"
+                    className="u-full-width"
                     value={this.state.firstName}
                     placeholder="First name"
-                    onChange={(e) =>
-                      this.setState({firstName: e.target.value,})
-                    }
+                    onChange={this.handleFieldChange}
                   />
-                  <input className="u-full-width"
-                    type="email"
+                  <input
+                    type="text"
+                    name="lastName"
+                    className="u-full-width"
                     value={this.state.LastName}
                     placeholder="Last name"
-                    onChange={(e) =>
-                      this.setState({lastName: e.target.value,})
-                    }
+                    onChange={this.handleFieldChange}
                   />
-                  <input className="u-full-width"
+                  <input
                     type="email"
-                    value={this.state.UserEmail}
-                    placeholder="E-mail"
-                    onFocus={this.removeEmail}
-                    onChange={(e) =>
-                      this.setState({UserEmail: e.target.value,})
-                    }
+                    name="email"
+                    className="u-full-width"
+                    value={this.state.email}
+                    placeholder="e-mail"
+                    onChange={this.handleFieldChange}
                   />
-                  <input type="text" className="u-full-width"
-                    value={this.state.UserName}
+                  <input
+                    type="text"
+                    name="userName"
+                    className="u-full-width"
+                    value={this.state.userName}
                     placeholder="Username"
-                    onChange={(e) =>
-                      this.setState({UserName: e.target.value,})
-                    }
+                    onChange={this.handleFieldChange}
                   />
-                  <input type="password" className="u-full-width"
-                    value={this.state.UserPassword1}
+                  <input
+                    type="password"
+                    name="password"
+                    className="u-full-width"
+                    value={this.state.password}
                     placeholder="Password"
-                    name="UserPassword1"
-                    onFocus={(e) => this.removePassword(e.target.name)}
-                    onChange={(e) => {
-                      this.setState({UserPassword1: e.target.value,})
-                    }}
+                    onChange={this.handleFieldChange}
                   />
-                  <input type="password" className="u-full-width" id="pass2"
-                    value={this.state.UserPassword2}
+                  <input
+                    type="password"
+                    name="password2"
+                    className="u-full-width"
+                    id="pass2"
+                    value={this.state.password2}
                     placeholder="Confirm password"
-                    name="UserPassword2"
-                    onFocus={(e) => this.removePassword(e.target.name)}
-                    onChange={(e) => {
-                      this.setState({UserPassword2: e.target.value,})
-                    }}
+                    onChange={this.handleFieldChange}
                   />
                 </div>
                 <div className="LoginSend">
-                  <div style={{ zIndex:1, }} className="ProfilePicBoxWrapper" onClick={() => this.setState({popped: true,})}>
-                    {this.imageChange(this.state.UserImage)}
+                  <div
+                    style={{ zIndex:1, }}
+                    className="ProfilePicBoxWrapper"
+                    onClick={() => this.setState({popped: true,})}
+                  >
+                    {this.renderImageChange(this.state.UserImage)}
                   </div>
                   <div className="SignupBox">
                     <div
-                      onClick={async () => {
-                        await this.arePasswordsTheSame(this.state)
-                        await this.checkEmailValid(this.state.UserEmail)
-                        await this.userSignupRequest(this.state)
-                      }}
+                      onClick={this.handleSignUpProcess}
                       className="nxt-btn-cp" style={{fontSize:'15px',}}
                     >Sign-up</div>
-                    {this.warningPassWordNotEqual(this.state.passwordWarning)}
-                    {this.warningEmailInvalid(this.state.emailWarning)}
+                    {this.renderPasswordWarning(this.state.passwordWarning)}
+                    {this.renderEmailWarning(this.state.emailWarning)}
                   </div>
                 </div>
               </div>
