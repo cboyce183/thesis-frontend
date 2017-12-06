e22a42512bddcfc781596dbea9c923be919da209
Lindsey <linrmz007@gmail.com>
2017-12-06 18:19:10 +0100

    style: styled about us

--- thesis/src/containers/Landing/AboutPersonal/AboutPersonal.js
+++ thesis/src/containers/Landing/AboutPersonal/AboutPersonal.js
@@ -18,27 +18,29 @@ class AboutPersonal extends Component {
         </div>
         <div className='landing-desc-per'>
           <div className="words-aboutper">
-            <p>
-              Zendama is easy! You receive an allocation of Zen to give to your colleagues every week. This is intended to be distributed by you as a reward for being awesome
-              and helping you or others out. You can give it for any deserving reason - maybe Alice helped you with a difficult client,
-              or Mo tidied up the breakroom when it was a complete mess. Zen is a gesture of good will for doing good deeds.
-            </p>
-            <p className='landing-desc-alt-txt spacer'>spacer</p>
-            <p>
-              "The Zen you in turn receive can be spent on perks provided by your place of work, such as food, drinks, event tickets,
-              or even gift vouchers for a luxury spa (although you'll have to negotiate this with your boss!). Accumulate good karma from being
-              a good employee, and get the rewards you deserve with your Zen. Everyones a winner!"
-            </p>
+            <div className='icon-personal'></div>
+            <div className='section1-personal-words'>
+              <p>
+                Zendama is easy! You receive an allocation of Zen to give to your colleagues every week. This is intended to be distributed by you as a reward for being awesome
+                and helping you or others out. You can give it for any deserving reason - maybe Alice helped you with a difficult client,
+                or Mo tidied up the breakroom when it was a complete mess. Zen is a gesture of good will for doing good deeds.
+              </p>
+              <p className='landing-desc-alt-txt spacer'>spacer</p>
+              <p>
+                "The Zen you in turn receive can be spent on perks provided by your place of work, such as food, drinks, event tickets,
+                or even gift vouchers for a luxury spa (although you'll have to negotiate this with your boss!). Accumulate good karma from being
+                a good employee, and get the rewards you deserve with your Zen. Everyones a winner!"
+              </p>
             </div>
-            <div className='section2-personal'>
+          </div>
+          <div className='section2-personal'>
             <div className='sec2-words'>
-            <p className='sign-up-works'>
-              'To sign up, your company will need to have an account (from which they can invite you to create your own account). See the corporate page to get started using Zendama in your place of work.'
-            </p>
-            <div className='landing-button-works' onClick={this.corporateRedir}>Corporate</div>
+              <p className='sign-up-works'>
+                'To sign up, your company will need to have an account (from which they can invite you to create your own account). See the corporate page to get started using Zendama in your place of work.'
+              </p>
+              <div className='landing-button-works' onClick={this.corporateRedir}>Corporate</div>
             </div>
-            </div>
-
+          </div>
         </div>
       </div>
     );
