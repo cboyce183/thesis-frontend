0664d9ec655d406bf1a73da173249958d30ca99a
Charles Boyce <wxaz020@live.rhul.ac.uk>
2017-12-06 15:00:56 +0100

    FEAT: jazzed it up

--- thesis/src/containers/Landing/Landing.js
+++ thesis/src/containers/Landing/Landing.js
@@ -1,7 +1,6 @@
 import React, { Component, } from 'react';
 import './Landing.css';
-// import { Route, } from 'react-router-dom';
-import {TweenMax, Elastic} from 'gsap';
+import {TweenMax, TweenLite, Elastic, TimelineMax, TimelineLite} from 'gsap';
 import ReactDOM from 'react-dom';
 import { Link, } from 'react-router-dom';
 import  base64  from 'base-64';
@@ -14,13 +13,18 @@ class Landing extends Component {
       password: null,
     }
   }
+  progAnimInit = () => {
+    const tl = new TimelineLite();
+    tl.from(ReactDOM.findDOMNode(this.throwit), 2, {drawSVG:0})
+      .from(ReactDOM.findDOMNode(this.catchit), 2, {drawSVG:0})
+    tl.timeScale(1);
+  }
   animateTitle = (target) => {
-    return TweenMax.to(ReactDOM.findDOMNode(target), 5, {color:'purple', yoyo:true, repeat:Infinity});
+    TweenMax.to(ReactDOM.findDOMNode(target), 5, {color:'purple', yoyo:true, repeat:Infinity});
   }
   animateHands = (target) => {
-    return TweenMax.to(ReactDOM.findDOMNode(target), 1, {marginBottom:'-60px', yoyo:true, repeat:Infinity});
+    TweenMax.to(ReactDOM.findDOMNode(target), 1, {marginBottom:'-60px', yoyo:true, repeat:Infinity, ease:'linear'});
   }
-
   loginRedir = () => window.location = '/login';
   corporateRedir = () => window.location = '/about_corporate';
   personalRedir = () => window.location = '/about_personal';
@@ -130,7 +134,7 @@ class Landing extends Component {
             </div>
           </div>
           <div className="MainSection">
-            <div className="SectionImage" style={{backgroundImage:`url(${require('../../assets/programming.svg')})`, marginRight: '60px', backgroundPosition: 'center',}}></div>
+            <div id="programming" className="SectionImage" style={{backgroundImage:`url(${require('../../assets/programming.svg')})`, marginRight: '60px', backgroundPosition: 'center',}} ref={e => {this.logogo = e;}}></div>
             <div className="MainSectionText">
               <p className='landing-desc-txt'>How does it work?</p>
               <p className='landing-desc-alt-txt'>So you're a tech nerd! Zendama uses blockchain technology to safely handle all Zen transactions within your company; our tech stack in general is pretty cool, so feel free to check it out below.</p>
@@ -141,52 +145,6 @@ class Landing extends Component {
         <div className="CorporateSection">
         </div>
       </div>
-      // <div className="landing-wrapper">
-      //   <div className="landing-title">
-      //     <img alt="" src={require('../../assets/zendomologo.png')} style={{maxHeight:'80px'}}/>
-      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
-      //     <h1 className="landing-title-text" style={{color:'#aaa',}}>zendama</h1>
-      //   </div>
-      //   <div className='landing-desc'>
-      //     <p className='landing-desc-txt'>Help your colleagues, receive rewards, share the love!</p>
-      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
-      //     <p className='landing-desc-alt-txt'>Zendama is a catalyst for harmony and friendship in the buisness place.</p>
-      //     <p className='landing-desc-alt-txt'>Get rewards from your place of work for being a good employee and helping out.</p>
-      //     <p className='landing-desc-alt-txt'>Reward those who help you with something that will really make a difference.</p>
-      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
-      //     <p className='landing-desc-alt-txt' style={{paddingBottom:'3vh',}}>Sounds good right?</p>
-      //     <input className="u-full-width" style={{width:'50%',}}
-      //       type="email"
-      //       value={this.state.email}
-      //       placeholder="Email"
-      //       onFocus={() => this.setState({noAccess: false, email: '',})}
-      //       onChange={(e) => this.setState({email: e.target.value,})}
-      //     />
-      //     <input type="password" className="u-full-width" style={{width:'50%', marginBottom:'3vh',}}
-      //       value={this.state.password}
-      //       placeholder="Password"
-      //       onFocus={() => this.setState({noAccess: false, password: '',})}
-      //       onChange={(e) => this.setState({password: e.target.value,})}
-      //     />
-      //     <div className='landing-button'style={{transform:'scale(1.2)',}} onClick={this.loginRequest.bind(this, this.state)}>Login</div>
-      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
-      //   </div>
-      //   <div className='landing-business'>
-      //     <h5 className='landing-desc-alt-txt'>Get Started</h5>
-      //     <p className='landing-desc-alt-txt spacer'>spacer</p>
-      //     <div style={{display:'flex',flexDirection:'row',}}>
-      //       <div className='landing-button' onClick={this.corporateRedir}>Corporate</div>
-      //       <div className='landing-button' onClick={this.personalRedir}>Personal</div>
-      //     </div>
-      //   </div>
-      //   <div className='landing-bottom-bar'>
-      //     <img alt="" src={require('../../assets/github-white.svg')} style={{maxHeight:'25px',}}/>
-      //     <p className='landing-desc-alt-txt smallspacer'>spaaaaa</p>
-      //     <img alt="" src={require('../../assets/facebook-black-icon.png')} style={{maxHeight:'35px',}}/>
-      //     <p className='landing-desc-alt-txt smallspacer'>spaaaaa</p>
-      //     <img alt="" src={require('../../assets/twitter.svg')} style={{maxHeight:'25px',}}/>
-      //   </div>
-      // </div>
     );
   }
 }
