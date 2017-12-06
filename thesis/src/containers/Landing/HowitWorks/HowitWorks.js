bf0afe775037980e184cdf6a6616c407eca2fe6c
Lindsey <linrmz007@gmail.com>
2017-12-06 20:16:52 +0100

    style: about-us final touch

--- thesis/src/containers/Landing/HowitWorks/HowitWorks.js
+++ thesis/src/containers/Landing/HowitWorks/HowitWorks.js
@@ -14,29 +14,28 @@ class HowitWorks extends Component {
     return (
       <div className="landing-wrapper">
         <div className='how-works-banner'>
-        <h1 className='how-label'>How does it work?</h1>
+          <h1 className='how-label'>How does it work?</h1>
         </div>
         <div className='how-container'>
-
           <div className='landing-desc-how'>
-          <div className='section1-personal'>
-          <div>
-          <p className='works-section1-label'> You as employer decide on a weekly allowance of Zen to give to your team.</p>
-            <ul className='corp-toggle-works'>
-              <li>They in turn can reward each other with Zen for good deeds.</li>
-              <li>You also provide one or more things that your team can buy with Zen: but heres the catch - they can only spend Zen they have received from others. Zen is designed to have no monetary value outside of the business, which helps ensure fair play and proper use of the Zendama system.</li>
-            </ul>
-            </div>
-            <div className='icon-howitworks'></div>
+            <div className='section1-personal'>
+              <div>
+                <p className='works-section1-label'> You as employer decide on a weekly allowance of Zen to give to your team.</p>
+                <ul className='corp-toggle-works'>
+                  <li>They in turn can reward each other with Zen for good deeds.</li>
+                  <li>You also provide one or more things that your team can buy with Zen: but heres the catch - they can only spend Zen they have received from others. Zen is designed to have no monetary value outside of the business, which helps ensure fair play and proper use of the Zendama system.</li>
+                </ul>
+              </div>
+              <div className='icon-howitworks'></div>
             </div>
             <div className='section2-works'>
-            <div className='section2-words'>
-            <p className='para-1'> How much Zen you choose to allocate each week, and how much rewards cost is completely down to you, so we provide you with a bespoke admin tool for managing your account.</p>
-            <p> It's really that simple - a happier workplace with less micromanagement, almost overnight.</p>
-            <Link to='/'>
-            <input type="submit" className='SectionDividerButton-works' value="Back" />
-            </Link>
-            </div>
+              <div className='section2-words'>
+                <p className='para-1'> How much Zen you choose to allocate each week, and how much rewards cost is completely down to you, so we provide you with a bespoke admin tool for managing your account.</p>
+                <p> It's really that simple - a happier workplace with less micromanagement, almost overnight.</p>
+                <Link to='/'>
+                  <input type="submit" className='SectionDividerButton-works' value="Back" />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
